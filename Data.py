ab2b7e863310e15722ae888ece601336bb331e2b
Cepheid <56135612+cepheidev@users.noreply.github.com>
2023-12-14 01:53:28 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -23,7 +23,6 @@ class Data:
  ├ /speedtest - Untuk Mengetes kecepatan server bot
  └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
 """
 
     close = [
@@ -49,9 +48,8 @@ class Data:
 
 @{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
 
- • Creator: @{}
+ • Creator: @bicitkntllu
  • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+👨‍💻 Develoved by </b><a href='https://t.me/bicitkntllu'>@bicitkntllu</a>
 """
